dc5a3e668a2b362d173222994df7910303d3150c
Aaditya Singh <119161277+Aditya13134@users.noreply.github.com>
2024-06-17 04:03:23 +0530

    Add files via upload

---
+++ index.mjs
@@ -0,0 +1,24 @@
+import random from 'random'
+import jsonfile from 'jsonfile'
+import moment from 'moment'
+import simpleGit from 'simple-git'
+const FILE_PATH='./data.json';
+
+const makeCommit = n => {
+    if(n===0) return simpleGit().push();
+    const x=random.int(0,54);
+    const y=random.int(0,6);
+
+    const Date=moment().subtract(1, 'y').add(1, 'd').add(x, 'w').add(y, 'd').format();
+
+    const data={
+        date:Date
+    }
+        console.log(Date);
+    jsonfile.writeFile(FILE_PATH,data , ()=>{
+        simpleGit().add([FILE_PATH]).commit(Date,{'--date': Date}, makeCommit.bind(this, --n));
+    });
+    
+}
+
+makeCommit(700);
